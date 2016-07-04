7a8722a0bc6bb3acc481960c20a69d40260a1e5b
Mentikora <notender_boy@ukr.net>
2016-07-05 02:18:49 +0300

    mobile menu, mainpage responsive

--- js/application.js
+++ js/application.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-	
+
+	// menu button & menu for mobile version
+	$('.menu_switch').on('click', function(){
+		$(this).toggleClass('active');
+		$('#nav_mobile').toggleClass('open fadeInDown');
+	});
+
 });
